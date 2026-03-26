038924f9e809f414cd3a179463081e7ee8d60dfb
Gabriel-Guanais <160058594+Gabriel-Guanais@users.noreply.github.com>
2026-03-26 13:40:33 -0400

    feat(ui): add image and style title and subtitle with stylesheet

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,40 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View>
-      <Text>Hello Gay</Text>
-      <Text>gabriel</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Image
+        source={require("@/assets/img1.png")}
+        style={styles.illustration}
+      />
+
+      <Text style={styles.title}>Entrar</Text>
+      <Text style={styles.subtitle}>Acesse sua conta com e-mail e senha.</Text>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FDFDFD",
+    padding: 32,
+  },
+
+  illustration: {
+    width: "100%",
+    height: 330,
+    resizeMode: "contain",
+    marginTop: 62,
+  },
+
+  title: {
+    fontSize: 32,
+    fontWeight: 900,
+  },
+
+  subtitle: {
+    fontSize: 16,
+  },
+});
