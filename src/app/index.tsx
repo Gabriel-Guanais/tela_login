2270c647de61f3010f3f4b433ab1710cd298e9ba
Gabriel-Guanais <160058594+Gabriel-Guanais@users.noreply.github.com>
2026-03-26 16:23:10 -0400

    feat(login): add ScrollView, style layout and implement password visibility toggle

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,34 @@
-import { Image, StyleSheet, Text } from "react-native";
+import { Input } from "@/components/input";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <SafeAreaView style={styles.container}>
-      <Image
-        source={require("@/assets/img1.png")}
-        style={styles.illustration}
-      />
+      <ScrollView
+        style={styles.scroll}
+        contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false} // esconde a barra de rolagem
+      >
+        <Image
+          source={require("@/assets/img1.png")}
+          style={styles.illustration}
+        />
 
-      <Text style={styles.title}>Entrar</Text>
-      <Text style={styles.subtitle}>Acesse sua conta com e-mail e senha.</Text>
+        <Text style={styles.title}>Entrar</Text>
+        <Text style={styles.subtitle}>
+          Acesse sua conta com e-mail e senha.
+        </Text>
+
+        <View style={styles.form}>
+          <Input
+            placeholder="E-mail"
+            keyboardType="email-address"
+            autoCapitalize="none"
+          />
+          <Input placeholder="Senha" secureTextEntry />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -19,22 +37,35 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#FDFDFD",
-    padding: 32,
+  },
+
+  scroll: {
+    flex: 1,
+  },
+
+  scrollContent: {
+    padding: 32, // O padding vem para cá, empurrando o conteúdo interno
+    paddingBottom: 64,
   },
 
   illustration: {
     width: "100%",
-    height: 330,
+    height: 320,
     resizeMode: "contain",
     marginTop: 62,
   },
 
   title: {
     fontSize: 32,
-    fontWeight: 900,
+    fontWeight: "bold",
   },
 
   subtitle: {
     fontSize: 16,
+  },
+
+  form: {
+    marginTop: 24,
+    gap: 16,
   },
 });
